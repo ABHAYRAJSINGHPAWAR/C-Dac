28713a53cd67f7456d3e0040b0824fa677162847
Abhay_Raj_Singh <abhayrajsinghpawar@gmail.com>
2024-03-07 00:11:36 +0530

    updated code

---
+++ c++/endl-in-async.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+using namespace std;
+
+int main()
+{
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    //  cout.tie(nullptr);
+    int arr[10];
+    for (int i = 0; i < 4; i++)
+    {
+
+        cin >> arr[i];
+        cout << arr[i] << "\n";
+    }
+    cout << "Hello";
+    printf("Caio");
+    cout << "World";
+    return 0;
+}
